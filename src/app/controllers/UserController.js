9c9be3c66ba90cfee9700b90155ca172bf3c49f4
Alexandre Feio <alefeio@gmail.com>
2020-02-27 16:31:32 -0300

    atualização de usuário

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -17,6 +17,35 @@ class UserController {
       provider
     })
   }
+
+  async update(req, res) {
+    const { email, oldPassword } = req.body
+
+    const user = await User.findByPk(req.userId)
+
+    if(email && email !== user.email) {
+      const userExists = await User.findOne({ where: { email }})
+
+      if(userExists) {
+        return res.status(400).json({ erro: 'Usuário já existe' })
+      }
+    }
+
+    if(oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ erro: 'Senha não confere!' })
+    }
+
+    const { id, name, provider } = await user.update(req.body)
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider
+    })
+
+    res.json({ ok: true })
+  }
 }
 
 export default new UserController()
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '9e50dec32ed0698bea085eeb0d2b5d2c',
+  expiresIn: '7d'
+}
