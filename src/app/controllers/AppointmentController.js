a085aea0d82e51aa856ffcbbf7c6bff818e5c26c
Alexandre Feio <alefeio@gmail.com>
2020-03-10 08:57:14 -0300

    Listagem de agendamentos de usuários e prestadores de serviço

---
+++ src/app/controllers/AppointmentController.js
@@ -0,0 +1,93 @@
+import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
+import Appointment from '../models/Appointment';
+import User from '../models/User';
+import File from '../models/File';
+
+class AppointmentController {
+  async index(req, res) {
+    const { page = 1 } = req.query
+
+    const appointments = await Appointment.findAll({
+      where: { user_id: req.userId, canceled_at: null },
+      order: ['date'],
+      limit: 20,
+      offset: (page - 1) * 20,
+      attributes: ['id', 'date'],
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'name'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['id', 'path', 'url']
+            },
+          ],
+        },
+      ],
+    });
+
+    res.json(appointments);
+  }
+
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      provider_id: Yup.number().required(),
+      date: Yup.date().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ erro: 'Falha na validação!' });
+    }
+
+    const { provider_id, date } = req.body;
+
+    // checando se o provider_id é um provider
+    const checkIsProvider = await User.findOne({
+      where: { id: provider_id, provider: true },
+    });
+
+    if (!checkIsProvider) {
+      return res
+        .status(401)
+        .json({ erro: 'Você não tem permissão para fazer agendamento.' });
+    }
+
+    // verifica se a data do agendamento é passada.
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res
+        .status(400)
+        .json({ erro: 'Não é possível agendar em uma data passada.' });
+    }
+
+    // verifica a disponibilidade da data
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return res
+        .status(400)
+        .json({ erro: 'Data não disponível para agendamento!' });
+    }
+
+    const appointment = await Appointment.create({
+      user_id: req.userId,
+      provider_id,
+      date: hourStart,
+    });
+
+    return res.json(appointment);
+  }
+}
+
+export default new AppointmentController();
--- src/routes.js
+++ src/routes.js
@@ -1,24 +1,33 @@
-import {Router} from 'express'
-import multer from 'multer'
-import multerConfig from './config/multer'
+import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
-import UserController from './app/controllers/UserController'
-import SessionController from './app/controllers/SessionController'
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController'
+import ScheduleController from './app/controllers/ScheduleController'
 
-import authMiddleware from './app/middlewares/auth'
+import authMiddleware from './app/middlewares/auth';
 
-const routes = new Router()
-const upload = multer(multerConfig)
+const routes = new Router();
+const upload = multer(multerConfig);
 
-routes.post('/users', UserController.store)
-routes.post('/sessions', SessionController.store)
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
-routes.use(authMiddleware)
+routes.use(authMiddleware);
 
-routes.put('/users', UserController.update)
+routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true })
-})
+routes.get('/providers', ProviderController.index);
 
-export default routes
+routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/appointments', AppointmentController.index)
+routes.post('/appointments', AppointmentController.store)
+
+routes.get('/schedules', ScheduleController.index)
+
+export default routes;
